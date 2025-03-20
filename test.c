a41d84b52a6d43329b4ff512422e6600f0f63f97
Algorithms <142917637+Algo1337@users.noreply.github.com>
2025-03-20 15:59:20 -0500

    Update test.c

--- test.c
+++ test.c
@@ -45,21 +45,17 @@ void test_page(cWS *server, cWR *req, WebRoute *route, int sock) {
     if(req->RequestType.Is(&req->RequestType, "POST")) {
         /* New Control */
         Control *newc = CreateControl(P_TAG, NULL, NULL, "nigbob", NULL);
+        free(newc->CSS);
         newc->CSS = NULL;
-        String t = newc->Construct(newc, 0, 0);
+        String t = newc->Construct(newc, 0, 1);
 
         /* Construct New Control */
         String test = NewString("{\"replace_elements\": {\"nigbob\": \"");
         test.AppendArray(&test, (const char *[]){t.data, "\"}}", NULL});
         test.data[test.idx] = '\0';
-        printf("%s\n", test.data);
-
-        // String test = ChangeElement(1, (char *[]){
-        //     {"nigbob", t.data},
-        //     NULL
-        // });
         
         SendResponse(server, sock, OK, DefaultHeaders, ((Map){0}), test.data);
+        
         test.Destruct(&test);
         t.Destruct(&t);
         newc->Destruct(newc, 0, 0);
@@ -78,29 +74,6 @@ void test_page(cWS *server, cWR *req, WebRoute *route, int sock) {
     free(template);
 }
 
-void event_handler(cWS *server, cWR *req, WebRoute *route, int sock) {
-    if(req->RequestType.Is(&req->RequestType, "POST")) {
-        if(FindKey(&req->Headers, "CF-Connecting-IP") || FindKey(&req->Headers, "cf-connecting-ip") || FindKey(&req->Headers, "x-forwarded-for"))
-            fetch_cf_post_data(server, req, sock);
-        
-        Map json = Decode_OneLine_JSON(req->Body.data);
-        char *route = FindKey(&json, "Route");
-        if(!route) {
-            printf("Error\n");
-            return;
-        }
-
-        int chk = SearchRoute(server, route);
-        if(chk > -1)
-        {
-            (void)(chk > -1 ? ((void (*)(cWS *, cWR *, WebRoute *, int))((WebRoute *)server->CFG.Routes[chk])->Handler)(server, req, server->CFG.Routes[chk], sock) : SendResponse(server, sock, OK, DefaultHeaders, ((Map){0}), "ERROR\n\n\n"));
-            return;
-        }
-
-        SendResponse(server, sock, OK, DefaultHeaders, ((Map){0}), "ERROR\n\n\n");
-    }
-}
-
 int main() {
     cWS *server = StartWebServer(NewString(""), 80, 0);
     if(!server)
@@ -109,12 +82,14 @@ int main() {
         return 1;
     }
 
-    server->AddRoutes(server, (WebRoute *[]){
-        &(WebRoute){ .Name = "index", .Path = "/", .Handler = test_page },
-        &(WebRoute){ .Name = "event_handler", .Path = "/event_handler", .Handler = event_handler },
+    WebRoute *routes[] = {
+        CreateRoute("index", "/", test_page),
         NULL
-    });
+    };
 
+    EnableLiveHandler(server);
+    server->AddRoutes(server, routes);
     server->Run(server, 999, 0);
+    server->Destruct(server);
     return 0;
 }
